1ffd8320b03d351d7cc042a412d148b23a589c09
LiamDavisonGates <49875459+LiamDavisonGates@users.noreply.github.com>
2021-12-03 03:43:38 +0000

    Update Counting_DNA_Nucleotides.py

--- Bioinformatics_Stronghold/Counting_DNA_Nucleotides.py
+++ Bioinformatics_Stronghold/Counting_DNA_Nucleotides.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title:   Counting DNA Nucleotides
+Rosalind ID:     DNA
+Rosalind #:      001
+URL:             http://rosalind.info/problems/dna/
+'''
 
