63f7206fd934ac953a6b86bc25936384187fd117
LiamDavisonGates <49875459+LiamDavisonGates@users.noreply.github.com>
2022-06-30 05:12:28 +0100

    Update Counting_DNA_Nucleotides.py

--- Bioinformatics_Stronghold/Counting_DNA_Nucleotides.py
+++ Bioinformatics_Stronghold/Counting_DNA_Nucleotides.py
@@ -9,3 +9,18 @@ Rosalind #:      001
 URL:             http://rosalind.info/problems/dna/
 '''
 
+with open('data/rosalind_dna.txt') as input_data:
+    dna_string = input_data.read().strip()
+
+    nucleotide_count = {}
+
+    for nucleotide in dna_string:
+        if nucleotide in nucleotide_count:
+            nucleotide_count[nucleotide] += 1
+        else:
+            nucleotide_count[nucleotide] = 1
+
+    print('{} {} {} {}'.format(nucleotide_count['A'],
+                               nucleotide_count['C'],
+                               nucleotide_count['G'],
+                               nucleotide_count['T']))
